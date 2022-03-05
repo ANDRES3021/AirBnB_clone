4b8f4b8072d61845e2dd373ed304f523cc478de3
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-03-05 10:40:01 -0500

    :c

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,13 +4,13 @@ Definimos el modulo FileStorage
 """
 import json
 import os
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage():
