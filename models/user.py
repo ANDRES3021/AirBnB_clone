d603204ead12d5c271aee46c53b017964c3f68e7
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-03-05 10:23:24 -0500

    user

--- models/user.py
+++ models/user.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-modulo que administra la clase usuario
-"""
+"""modulo que administra la clase usuario"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    clase que representa usuario
-    """
+    """Clase que representa usuario"""
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """
-            Sends arguments to the parent class and create a new instance
-        """
+        """Función para crear una nueva instancia"""
         super().__init__(*args, **kwargs)
